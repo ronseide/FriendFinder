492e97e9d15c8fc56161433f412e25ac2f259014
ronseide <ron.seide@gmail.com>
2019-04-25 20:29:23 -0400

    final

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,43 +1,10 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
 var surveyData = require("../data/friends");
-
-// var waitListData = require("../data/waitinglistData");
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
-  // API GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-  // ---------------------------------------------------------------------------
-
   app.get("/api/friends", function(req, res) {
 
     res.json(surveyData);
   });
-
-
-  // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
-  // ---------------------------------------------------------------------------
-
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
-    // It will do this by sending out the value "true" have a table
-    // req.body is available since we're using the body parsing middleware
-    // if (tableData.length < 5) {
       var newUser = req.body;
       var userScores = newUser.scores;
       var bestMatch = {
@@ -62,11 +29,6 @@ module.exports = function(app) {
       res.json(bestMatch);
       console.log("bestMatch", bestMatch);
   });
-
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
-
   // app.post("/api/clear", function(req, res) {
   //   // Empty out the arrays of data
   //   friends.length = [];
